4fae6316ea36e68755ed4d35f2d4eb7ad2c03e55
satish <seerpini@purdue.edu>
2012-04-17 22:20:07 -0400

    Commint the file with the actual 'poweroff' shell command code

---
+++ src/shell/xsh_poweroff.c
@@ -0,0 +1,29 @@
+/* xsh_poweroff.c - xsh_poweroff*/
+
+#include <xinu.h>
+#include <stdio.h>
+
+/*------------------------------------------------------------------------
+ * xsh_poweroff - shell command to delay for a specified number of seconds
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_poweroff(int nargs, char *args[])
+{
+
+
+	struct	procent	*prptr;		/* pointer to process		*/
+        pid32   i;
+
+        /* Kill each process  not including the null process  */
+	for (i = 1; i < NPROC; i++) {
+		prptr = &proctab[i];
+		if (prptr->prstate == PR_FREE || i == currpid) {  /* skip unused slots	*/
+			continue;
+		}
+                printf("Killing process with pid [%d]\r\n", i);
+                kill(i);
+	}
+
+        printf("Killing all the processes successfull, now killing shell and quitting...\r\n");
+        kill(currpid);
+}
